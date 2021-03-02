4ccecc7f60e0029034ef2ebf9273ed47ac4f8b64
Kouki Kyoguro <kyogurokouki@Kouki-KyoguronoMacBook-Pro.local>
2021-03-02 18:50:22 +0900

    set up

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c8394b5d88e863d1c0a24386e13e6ff6c91453a4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c8394b5d88e863d1c0a24386e13e6ff6c91453a4',
+    ),
+  ),
+);

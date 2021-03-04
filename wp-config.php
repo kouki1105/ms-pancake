c96471d46eb2bfcad936d36312b8eea0b6291b2c
Kouki Kyoguro <kyogurokouki@Kouki-KyoguronoMacBook-Pro.local>
2021-03-04 12:39:25 +0900

    set up

--- wp-config.php
+++ wp-config.php
@@ -19,41 +19,23 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-// /** The name of the database for WordPress */
-// define( 'DB_NAME', 'local' );
-
-// /** MySQL database username */
-// define( 'DB_USER', 'root' );
-
-// /** MySQL database password */
-// define( 'DB_PASSWORD', 'root' );
-
-// /** MySQL hostname */
-// define( 'DB_HOST', 'localhost' );
-
-// /** Database Charset to use in creating database tables. */
-// define( 'DB_CHARSET', 'utf8' );
-
-// /** The Database Collate type. Don't change this if in doubt. */
-// define( 'DB_COLLATE', '' );
-$url = parse_url(getenv('CLEARDB_DATABASE_URL'));
 /** The name of the database for WordPress */
-define('DB_NAME', trim($url['path'], '/'));
+define( 'DB_NAME', 'local' );
 
 /** MySQL database username */
-define('DB_USER', $url['user']);
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define('DB_PASSWORD', $url['pass']);
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define('DB_HOST', $url['host']);
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define( 'DB_COLLATE', '' );
 
 /**
  * Authentication Unique Keys and Salts.
